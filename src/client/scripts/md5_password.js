6ddea313bac1dc649aa75f03668d92896495654e
arshappleid <arsh.appleid@gmail.com>
2024-02-08 18:09:14 -0800

    added code for md5 password

---
+++ src/client/scripts/md5_password.js
@@ -0,0 +1,18 @@
+<script src="https://cdnjs.cloudflare.com/ajax/libs/crypto-js/4.0.0/crypto-js.min.js"></script>;
+
+document.getElementById("myForm").addEventListener("submit", function (e) {
+    e.preventDefault(); // Prevent the form from submitting
+
+    // Get the password from the input
+    var password = document.getElementById("password").value;
+
+    // Hash the password using MD5
+    var hashedPassword = CryptoJS.MD5(password).toString();
+
+    // You can either send the hashed password as a hidden input or replace the password field value
+    // For this example, let's replace the password field value
+    document.getElementById("password").value = hashedPassword;
+
+    // Now submit the form programmatically
+    this.submit();
+});
