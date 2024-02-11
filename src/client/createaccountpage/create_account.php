c9ef4b1afb1f4a3ed030784ce475b30c294655f3
arshappleid <arsh.appleid@gmail.com>
2024-02-10 20:53:37 -0800

    Delete THis

--- src/client/createaccountpage/create_account.php
+++ src/client/createaccountpage/create_account.php
@@ -2,12 +2,12 @@
 <!DOCTYPE html>
 <html lang="en">
   <head>
-    <title>Login Page</title>
+    <title>Create Account</title>
     <meta charset="UTF-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <link rel="stylesheet" href="loginstyles.css" />
+    <link rel="stylesheet" href="createaccountstyles.css" />
     <link href="bootstrap3_defaultTheme/dist/css/bootstrap.css"
 		rel="stylesheet">
   </head>
@@ -29,17 +29,23 @@
         </div>
       </div>
       <div class="triangleextendblack"> 
-        <form id = "loginForm" method="POST" action="./../../server/verify_user_login.php">
-          <input type="email" id="email" name="email" placeholder="E-mail">
-          <input type="password" id="password" name="password" placeholder="Password">
+        <form id = "createAccountForm" method="POST" action="./../../server/create_user.php">
+
+          <input type="email" id="email" name="email" placeholder="E-mail" required>
+
+          <input type="password" id="password" name="password" placeholder="Password" required>
+
+          <input type="password" id="password2" name="password2" placeholder="Re-enter Password" required>
+
+          <input type="text" id="firstName" name="firstName" placeholder="First Name" required>
+
+          <input type="text" id="lastName" name="lastName" placeholder="Last Name" required>
+
+          <input type="file" id="profilePicture" name="profilePicture" placeholder="Upload Profile Picture" accept="image/*">
+
           <div class="button-container">
-            <button type="submit">Login</button>
+            <button type="submit">Create Account</button>
           </div>
-<<<<<<< HEAD
-          <a class="accounttext" href="#">Need an account?</a>
-=======
-          <a class="accounttext" href="./../createaccountpage/create_account.php">Need an account?</a>
->>>>>>> c08fc60 (testing upload to specified branch)
         </form> 
       </div>
       <div class="triangle-element"></div>     
@@ -47,17 +53,20 @@
     <div class="footerblack"></div>
 
     <script>
-      document.getElementById("loginForm").addEventListener("submit", function (e) {
+      document.getElementById("createAccountForm").addEventListener("submit", function (e) {
         e.preventDefault();
         var email = document.getElementById("email").value;
         var password = document.getElementById("password").value;
+        var password2 = document.getElementById("password2").value;
+        var firstName = document.getElementById("firstName").value;
+        var lastName = document.getElementById("lastName").value;
+        var profilePicture = document.getElementById("profilePicture").value;
 
-        if (!email || !password) {
-            alert('Please enter both email and password.');
+        if (password !== password2) {
+            alert("Passwords Don't Match");
             return;
         }
 
-        var password = document.getElementById("password").value;
         var hashedPassword = CryptoJS.MD5(password).toString();
         document.getElementById("password").value = hashedPassword;
         this.submit();
