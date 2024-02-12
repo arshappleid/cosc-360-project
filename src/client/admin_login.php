de2ec9e546a7234779a96a3cab8a31ae2a8b63a5
arshappleid <arsh.appleid@gmail.com>
2024-02-11 23:03:20 -0800

    Added Admin login capabilities

---
+++ src/client/admin_login.php
@@ -0,0 +1,66 @@
+<?php session_start(); ?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<title>Login Page</title>
+	<meta charset="UTF-8" />
+	<meta http-equiv="X-UA-Compatible"
+		content="IE=edge" />
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/crypto-js/4.0.0/crypto-js.min.js"></script>
+	<meta name="viewport"
+		content="width=device-width, initial-scale=1.0" />
+	<link rel="stylesheet"
+		href="./css/admin_login.css" />
+	<link href="bootstrap3_defaultTheme/dist/css/bootstrap.css"
+		rel="stylesheet">
+</head>
+
+<body style="text-align:center;">
+	<div>
+		<h3>Admin Login</h3>
+		<form id="login_form"
+			method="POST"
+			action="./../server/validate_admin.php">
+			<input class="input_field"
+				placeholder="E-mail"
+				type="text"
+				name="email">
+			<input class="input_field"
+				placeholder="Password"
+				type="password"
+				name="password">
+			<?php
+			if (isset($_SESSION['MESSAGE'])) {
+				echo "<h4 style = \"color:red;\">" . $_SESSION['MESSAGE'] . "</h4>";
+				unset($_SESSION['MESSAGE']);
+			}
+			?>
+			<div id="button_holder">
+				<button type="submit">Login</button>
+				<button type="reset">Reset</button>
+			</div>
+		</form>
+	</div>
+
+	<script>
+	document.getElementById("login_form").addEventListener("submit", function(e) {
+		e.preventDefault();
+		var email = document.getElementById("login_form").elements['email'].value;
+		var password = document.getElementById("login_form").elements['password'].value;
+
+		if (!email || !password) {
+			alert('Please enter both email and password.');
+			return;
+		}
+		var hashedPassword = CryptoJS.MD5(password).toString();
+		document.getElementById("login_form").elements['password'].value = hashedPassword;
+		this.submit();
+	});
+	</script>
+
+
+</body>
+
+</html>
