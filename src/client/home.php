b95bfc63a85569a239149f9713485e630dd1720c
arshappleid <arsh.appleid@gmail.com>
2024-02-11 23:54:07 -0800

    Added Basic Layout For home page

--- src/client/home.php
+++ src/client/home.php
@@ -9,8 +9,28 @@
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/crypto-js/4.0.0/crypto-js.min.js"></script>
 	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+	<link rel="stylesheet" href="./css/global.css" />
 	<link rel="stylesheet" href="./css/home.css" />
-	<link href="bootstrap3_defaultTheme/dist/css/bootstrap.css" rel="stylesheet">
 </head>
+<!-- Refer to https://www.w3schools.com/html/html_layout.asp for Layout Design -->
 
-<body style="text-align:center;">
+<body class="default_body">
+	<header>Welcome to our Grocery Tracker</header>
+	<nav>
+		<a href="#">Home</a>
+		<a href="#">Logout</a>
+	</nav>
+	<div id="search_bar">SearchBAr</div>
+	<section>
+		<aside>
+			<h3>Item1</h3>
+		</aside>
+		<article>
+			<h3>Item 1 Comment</h3>
+		</article>
+	</section>
+	<footer>
+		<p>Footer</p>
+	</footer>
+
+</body>
