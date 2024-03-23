ad4e375ed642c369e446f90368819d62b584baa3
arshappleid <arsh.appleid@gmail.com>
2024-03-23 00:46:41 -0700

    Updated Database Credents

--- src/server/functions/db_connection.php
+++ src/server/functions/db_connection.php
@@ -1,8 +1,8 @@
 <?php
-$host = "localhost";  // docker container name
-$database = "db_68424464";
-$user = "68424464";   // i created a new user using this login for the db
-$password = "68424464";
+$host = "mysql-server";  // docker container name
+$database = "market_database";
+$user = "root";   // i created a new user using this login for the db
+$password = "secret";
 $connection = mysqli_connect($host, $user, $password, $database);
 
 // Check connection
