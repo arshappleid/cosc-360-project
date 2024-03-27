f6a0ce4165b5dae2f51141433a970d6a9f4cb303
SpaceJamDVD <97553001+SpaceJamDVD@users.noreply.github.com>
2024-03-27 08:37:53 -0700

    test

--- src/server/validate_user.php
+++ src/server/validate_user.php
@@ -3,17 +3,18 @@ session_start();
 require_once './functions/User_management.php';
 $email = $_POST['email'];
 $hashedPassword = $_POST['password'];
-
-if (isset($email) && isset($hashedPassword)) {
-	if (User_management::validateUserLogin($email, $hashedPassword) == "VALID_LOGIN") {
-		//print_r("shit not broken");
-		$_SESSION['USER_EMAIL'] = $email;
-		header('Location: ../client/home.php');
-		exit();
-	} else {
-		$_SESSION['MESSAGE'] = 'INVALID LOGIN';
-		header('Location: ../client/login.php');
-		exit();
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+	if (isset($email) && isset($hashedPassword)) {
+		if (User_management::validateUserLogin($email, $hashedPassword) == "VALID_LOGIN") {
+			//print_r("shit not broken");
+			$_SESSION['USER_EMAIL'] = $email;
+			header('Location: ../client/home.php');
+			exit();
+		} else {
+			$_SESSION['MESSAGE'] = 'INVALID LOGIN';
+			header('Location: ../client/login.php');
+			exit();
+		}
 	}
 }
 
