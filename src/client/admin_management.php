9df45d9359daf718858855478f069590aabd661f
Prabhmeet Singh Deol <68478842+arshappleid@users.noreply.github.com>
2024-03-29 00:14:07 -0700

    Added Admin Management Page

--- src/client/admin_management.php
+++ src/client/admin_management.php
@@ -8,7 +8,7 @@ if (!isset($_SESSION['BREADCRUMBS'])) {
 	$_SESSION['BREADCRUMBS'] = array();
 }
 
-$current_page = ["home", "./home.php"];
+$current_page = ["Admin Management", "./admin_management.php"];
 $last_item_index = count($_SESSION['BREADCRUMBS']) - 1;
 
 // Add the current page only if it's not the last one already in the breadcrumb trail
@@ -16,12 +16,11 @@ if ($last_item_index < 0 || $_SESSION['BREADCRUMBS'][$last_item_index][0] != $cu
 	array_push($_SESSION['BREADCRUMBS'], $current_page);
 }
 ?>
-
 <!DOCTYPE html>
 <html lang="en">
 
 <head>
-	<title>Banana Hammock</title>
+	<title>Admin Management</title>
 	<meta charset="UTF-8" />
 	<meta http-equiv="X-UA-Compatible" content="IE=edge" />
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
@@ -29,21 +28,17 @@ if ($last_item_index < 0 || $_SESSION['BREADCRUMBS'][$last_item_index][0] != $cu
 	<script type="text/javascript" src="./scripts/home.js"></script>
 	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 	<link rel="stylesheet" href="css/global.css" />
-	<link rel="stylesheet" href="css/home.css" />
+	<link rel="stylesheet" href="css/admin_management.css" />
 
 </head>
 
 <body>
-
 	<div class="container">
 		<div class="headerblack">
 			<a href="home.php" class="home-button">Home</a>
 			<?php
 			if (isset($_SESSION['USER_EMAIL']) || isset($_SESSION['ADMIN_EMAIL'])) {
 				echo "<div style = \"display:flex;\">";
-				if (isset($_SESSION['ADMIN_EMAIL'])) {
-					echo "<a href=\"admin_management.php\" class=\"login-button\" >Admin Management</a>";
-				}
 				echo "<a href=\"account_page.php\" class=\"login-button\">";
 				//echo "<img id = \"avatar_img\" src = \"./../server/getUserImages.php>";
 				echo "Account</a>";
@@ -63,62 +58,9 @@ if ($last_item_index < 0 || $_SESSION['BREADCRUMBS'][$last_item_index][0] != $cu
 			}
 			?>
 		</div>
-
-		<div class="headeryellow">
-			<div class="search-container">
-				<input type="text" id="search-input" placeholder="Search...">
-				<?php
-				$stores = Item_info::getAllStoreList();
-				if (count($stores) == 0) {
-					echo $stores;
-				} else {
-					echo "<select id = \"store_select\" class=\"select_dropdown\">";
-					echo "<option value=\"all\">All Stores</option>";
-					foreach ($stores as $key => $store) {
-						echo "<option value=\"" . $store['STORE_ID'] . "\" >" . $store['STORE_NAME'] . "</option>";
-					}
-					echo "</select>";
-				}
-				?>
-				<button type="button" id="search-button">Search</button>
-			</div>
-		</div>
-
 		<?php include_once './../server/breadcrumbs.php' ?>
-
-		<div class="underheadercontainer">
-			<div class="overlay">
-				<?php
-				echo "<div id = \"item_list\"></div>";
-				//echo '<pre>' . print_r($_SESSION, true) . '</pre>';
-				?>
-			</div>
-			<div class="triangleextendblack"></div>
-			<div class="triangle-element"></div>
-		</div>
 	</div>
-	<footer>
-		<div>
-			<nav>
-				<ul>
-					<li><a href="home.php">Home</a></li>
-					<?php
-					if (isset($_SESSION['USER_EMAIL']) || isset($_SESSION['ADMIN_EMAIL'])) {
-
-						echo "<li><a href=\"account_page.php\">Account</a></li>";
-					} else {
-						echo "<li><a href=\"create_account.php\">Create Account</a></li>
-						<li><a href=\"login.php\">Login</a></li>
-						<li><a href=\"admin_login.php\">Admin Login</a></li>";
-					}
-					?>
-				</ul>
-				<p>&copy; Banana Hammock 2024</p>
-			</nav>
-		</div>
-	</footer>
-	<?php
-	echo "<script type=\"text/javascript\" src=\"./scripts/home.js\"></script>";
-	?>
-
+	<div>
+		<?php include_once './../server/admin_management/display_all_users.php' ?>
+	</div>
 </body>
---
+++ src/server/admin_management/display_all_users.php
@@ -0,0 +1,20 @@
+<?php
+include_once './../functions/admin_management.php';
+if (isset($_GET["ban_user"])) {
+	Admin_management::toggleBanUserAccount($_GET["ban_user"]);
+	unset($_GET["ban_user"]);
+}
+
+$users = Admin_management::getAllUsers();
+echo "<table>";
+echo "<tr><th>First Name</th><th>Last Name</th><th>Email</th><th>Ban Status</th></tr>";
+foreach ($users as $user) {
+	echo "<tr>";
+	echo "<td>" . $user['Email'] . "</td>";
+	echo "<td>" . $user['First_Name'] . "</td>";
+	echo "<td>" . $user['Last_Name'] . "</td>";
+	echo "<td>" . ($user['BANNED_STATUS'] == 1 ? "Banned" : "Active") . "</td>";
+	echo "<td>" . "<a href=\"./display_all_users.php?ban_user=" . $user['Email'] . "\">Toggle Ban</a></td>";
+	echo "</tr>";
+}
+echo "</table>";
