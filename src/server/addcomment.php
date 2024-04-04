e2e84a76a8d85f7ba423cd5c7fabe6fb7c4424e8
Prabhmeet Singh Deol <68478842+arshappleid@users.noreply.github.com>
2024-04-04 13:07:52 -0700

    Currently there is a bug , that prevents Comments from being added in the database, have tested api call , and wrote tests and they are working fine

--- src/server/addcomment.php
+++ src/server/addcomment.php
@@ -4,7 +4,6 @@ try {
 	if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 		if (isset($_POST['COMMENT_TEXT']) & isset($_POST['ITEM_ID']) & isset($_POST['USER_EMAIL'])) {
 			if ($_POST['COMMENT_TEXT'] != "") {
-				print_r($_POST) . "<br>";
 				$resp = Comments::addComment($_POST['COMMENT_TEXT'], $_POST['ITEM_ID'], $_POST['USER_EMAIL']);
 			}
 		}
@@ -12,4 +11,5 @@ try {
 } catch (Exception $e) {
 	echo "<script>console.error(" . json_encode($e->getMessage()) . ");</script>";
 }
-#header('Location: ./../client/home.php');
+header('Location: ./../client/home.php');
+exit;
