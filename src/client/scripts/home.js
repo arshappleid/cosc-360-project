b135338603f98f52826b5c76cf55ccb8e4c1d24a
SpaceJamDVD <97553001+SpaceJamDVD@users.noreply.github.com>
2024-04-11 12:55:35 -0700

    server

--- src/client/scripts/home.js
+++ src/client/scripts/home.js
@@ -50,36 +50,5 @@ $(document).ready(function () {
             }
         });
     }
-
-    function initializeCollapsibles() {
-        $(".collapsible").off("click").on("click", function () { // Turn off any existing click handlers to prevent duplicates
-            var sibling = $(this).prev().children().first().next().children().first();
-            var siblings = [];
-            while (sibling.length !== 0) {
-                if (!sibling.is(this)) {
-                    siblings.push(sibling.get(0));
-                }
-                sibling = sibling.next();
-            }
-            if (siblings.length === 0) return;
-            siblings.forEach((content) => {
-                if ($(content).hasClass("hidden_comments")) {
-                    $(content).toggle(); // This will handle showing and hiding
-                }
-            });
-            if (this.innerHTML === "Show All Comments") {
-                this.innerHTML = "Hide Comments";
-            } else {
-                this.innerHTML = "Show All Comments";
-            }
-        });
-    }
-
-    initializeCollapsibles();
-
-    $(window).on("pageshow", function(event) {
-        if (event.originalEvent.persisted) {
-            initializeCollapsibles(); 
-        }
-    });
+    
 });
